b4c1fd92a03e535b223e8689625337136833d5ed
Cliff Minks <Cliff.Minks@gmail.com>
2016-02-29 19:34:11 -0600

    Add forget user method and test logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,15 @@ class User < ActiveRecord::Base
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
+
+  # Returns true if given token matches
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
